f400bca27cf3edb9b224952f3ecee4689c29060d
Alejandro Portilla Gainza <aportiga@mercadona.es>
2024-02-18 22:00:38 +0100

    feat: Simple solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -5,9 +5,30 @@ package org.tfoc;
  */
 public class Solution {
 
-    public Integer maxSubArray(Integer[] nums) {
+    public static Integer maxSubArray(int[] nums) {
 
-        return null;
+        int sumTotal = nums[0];
+        int currentOffset = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            int subSumTotal = sumTotal;
+            if (nums[i] > sumTotal) {
+                sumTotal = nums[i];
+                currentOffset = i;
+            } else {
+                if (nums[i] + sumTotal > sumTotal) {
+                    while (currentOffset < i) {
+                        currentOffset = currentOffset + 1;
+                        subSumTotal = subSumTotal + nums[currentOffset];
+                    }
+                    if (subSumTotal > sumTotal) {
+                        sumTotal = subSumTotal;
+                        currentOffset = i;
+                    }
+                }
+            }
+        }
+        return sumTotal;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,31 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 @Slf4j
 class SolutionTest {
 
-    @Test
-    void testSolution() {
+    private static Stream<Arguments> provideInputNums() {
+        return Stream.of(
+                Arguments.of(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}, 6),
+                Arguments.of(new int[]{1}, 1),
+                Arguments.of(new int[]{5, 4, -1, 7}, 15),
+                Arguments.of(new int[]{5, 4, -1, -3, -2, -8}, 9)
+        );
+    }
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    @ParameterizedTest
+    @MethodSource("provideInputNums")
+    void testSolution(int[] nums, Integer sumExpected) {
+        Integer sum = Solution.maxSubArray(nums);
+
+        Assertions.assertEquals(sumExpected, sum);
     }
 
 }
